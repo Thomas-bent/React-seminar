817ff80c1e16ddb63212696a6defd46d052c0d47
Thomas Bent <thomas.bentlohner@outlook.de>
2022-11-26 15:51:23 +0100

    header init

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
+import { Header } from "./Header";
+import { Routes, Route } from "react-router-dom";
+import { Homepage } from "./Homepage";
+
 function App() {
   return (
     <div className="App">
-      
+      <Header />
+      <Routes>
+        <Route path="/" element={ <Homepage /> }/>
+      </Routes>
     </div>
   );
 }
---
+++ src/Header.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import Style from './Header.module.css' 
+
+export const Header = () => {
+  return (
+    <div className={ Style.navbar_container }>
+        <div className="logo">
+            asdfasd
+        </div>
+        <ul className={ Style.navbar_list }>
+          <li className={ Style.topic }><Link to="/"><p className={ Style.directlinks }>Beiträge</p></Link></li>
+          <li className={ Style.nav_spacer }></li>
+          <li className={ Style.topic }><Link to="/">Backstage</Link></li>
+          <li className={ Style.nav_spacer }></li>
+          <li className={ Style.topic }><Link to="/">Über Uns</Link></li>
+        </ul>
+    </div>
+  )
+}
