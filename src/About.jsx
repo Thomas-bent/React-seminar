dad79a9c34030701205d8d11c564c0579e434fad
Thomas Bent <thomas.bentlohner@outlook.de>
2022-12-17 14:55:21 +0100

    yt video format changed (mobile)

--- src/About.jsx
+++ src/About.jsx
@@ -3,8 +3,8 @@ import Style from './About.module.css'
 
 export const About = () => {
   return (
-          <div className={Style.main_container}>
-        <iframe width="1280" height="720" src="https://www.youtube-nocookie.com/embed/M44xgfhPAYw" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+          <div className={ Style.main_container }>
+        <iframe className={ Style.video } width="1280" height="720" src="https://www.youtube-nocookie.com/embed/M44xgfhPAYw" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
     </div>
   )
 }
