71a91a4831420d8fb746351235a13fd7700aedc1
Thomas Bent <thomas.bentlohner@outlook.de>
2022-12-17 14:48:25 +0100

    YT video hinzugefügt

--- src/About.jsx
+++ src/About.jsx
@@ -1,7 +1,10 @@
 import React from 'react'
+import Style from './About.module.css'
 
 export const About = () => {
   return (
-    <div>About</div>
+          <div className={Style.main_container}>
+        <iframe width="1280" height="720" src="https://www.youtube-nocookie.com/embed/M44xgfhPAYw" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+    </div>
   )
 }
